ede3af7caecaf0b3af959405b4ce270b5b9086f1
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-08-11 11:27:44 +0530

    Add files via upload

---
+++ EvenoddDemo.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.Scanner;
+class EvenoddDemo
+{
+    public static void main(String args[])
+    {
+        int a;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter a:");
+        a=sc.nextInt();
+        if(a%2==0)
+        {
+            System.out.println(a+"is even");
+        }
+        else
+        {
+            System.out.println(a+"is odd");
+        }
+    }
+
+}
