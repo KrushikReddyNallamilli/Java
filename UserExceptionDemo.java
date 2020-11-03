cc5a9779095b5095bbfb8416dfda8669b596475d
Nallamilli S V S R Krushik Reddy <52321852+KrushikReddyNallamilli@users.noreply.github.com>
2020-11-03 08:46:39 +0530

    Add files via upload

---
+++ UserExceptionDemo.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class NotEligibleException extends Exception 
+
+{
+	
+	public String toString()
+
+	{
+
+		return"You are not eligibe for voting...";
+
+	}
+
+}
+
+class UserExceptionDemo
+
+{
+	
+public static void main(String[] args)
+	
+{
+		
+	Scanner sc= new Scanner(System.in);
+
+		try
+		
+		{
+
+			System.out.print("Enter your age:");
+
+			int age=sc.nextInt();
+
+			if(age<18)
+			{
+
+				throw new NotEligibleException();
+
+			}
+			else
+			{
+
+				System.out.println("Welcome! You are eligible for voting!");
+
+			}
+
+		}
+
+		catch(NotEligibleException ne)
+
+		{
+
+			System.out.println(ne);
+
+		}
+
+	}
+
+}
